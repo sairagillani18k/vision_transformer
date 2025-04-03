188d70547ce1f6e98c3aca45fffdf908c5442b7d
Saira Gillani <58274863+sairagillani18k@users.noreply.github.com>
2025-04-03 23:22:16 +0500

    Create dataset.py

---
+++ utils/dataset.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.data import Dataset
+
+class GestureDataset(Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.data = torch.tensor(data, dtype=torch.float32)
+        self.labels = torch.tensor(labels, dtype=torch.long)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = self.data[idx]
+        y = self.labels[idx]
+        return x, y
